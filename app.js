859e7e46995481a0642cbf82b569592583846bee
handiXu <xuhandi2013@gmail.com>
2015-10-11 22:18:59 -0400

    [D3] load bar chart data from server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var d3 = require('d3');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
